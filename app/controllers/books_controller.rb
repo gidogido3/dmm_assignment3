1d8497f8b50b22465746cbe315f0574f37642e2a
miso <129868570+gidogido3@users.noreply.github.com>
2023-06-16 17:36:54 +0000

    [Update] RSpec完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,77 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+
   def new
     @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current.id
-    @book.save
-    redirect_to book_path
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@new_book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @user = current_user
+    @new_book = Book.new
+    @books = Book.all
   end
 
   def show
+    @new_book = Book.new
+    @book = Book.find(params[:id])
+    @current_user = current_user
+    @user = @book.user
+    # @user = @book.user
+  end
+
+  def edit
+    @edit_book = Book.find(params[:id])
+    if @edit_book.user != current_user
+      @user = current_user
+      @new_book = Book.new
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def update
+    @edit_book = Book.find(params[:id])
+
+    if @edit_book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@edit_book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:shop_name, :image, :caption)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    @book = Book.find(params[:id])
+    user_id = @book.user.id
+    login_user_id = current_user.id
+    if (user_id != login_user_id )
+      redirect_to books_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,50 @@
 class UsersController < ApplicationController
-  def edit
-  end
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
+    @user = current_user
+    @users = User.all
+    @new_book = Book.new
   end
 
   def show
+    @new_book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
   end
+
+  def edit
+    @edit_user = User.find(params[:id])
+    if @edit_user != current_user
+      @user = current_user
+      @users = User.all
+      render :index
+    end
+  end
+
+  def update
+    @edit_user = User.find(params[:id])
+    if @edit_user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@edit_user.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if (user_id != login_user_id )
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
